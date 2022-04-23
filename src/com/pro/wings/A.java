921113456004df451991a60c6afa5453acd496c2
JD <jayprowings@gmail.com>
2022-04-23 12:23:33 +0530

    first commit

---
+++ src/com/pro/wings/A.java
@@ -0,0 +1,15 @@
+package com.pro.wings;
+
+public class A {
+
+    public static void main(String[] args) {
+
+        add(10,20);
+    }
+
+    public static void add(int n1, int n2)
+    {
+        int res = n1+n2;
+        System.out.println(res);
+    }
+}
